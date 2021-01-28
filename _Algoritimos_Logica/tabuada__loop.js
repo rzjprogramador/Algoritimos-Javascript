76f6e7f3a530e4aef84b25db8d536ea9c65a7144
rzjprogramador <rzjprogramador@gmail.com>
2021-01-27 22:51:07 -0300

    tABUADA LOOP FOR

---
+++ _Algoritimos_Logica/tabuada__loop.js
@@ -0,0 +1,11 @@
+// TABUADA COM LOOP FOR
+
+ // numero a ser afeytado por operacao
+ valor = 3
+
+ limiteTabuada = 11
+ 
+ for(i=0; i < limiteTabuada;i++) {
+   //console.log(valor + "x" + a + " = " + (valor * a))
+   console.log(`${valor} x ${i} = ${(valor * i)}`)
+ }
