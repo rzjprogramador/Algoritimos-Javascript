46bc29f0fd2348a73e7a9f33725c01fc42029b62
rzjprogramador <rzjprogramador@gmail.com>
2021-02-08 13:08:32 -0300

    Reduce

--- Javascript/reduce/reduce.js
+++ Javascript/reduce/reduce.js
@@ -1,18 +1,10 @@
+/*
+  DESAFIO : SOMAR TODOS NUMEROS DO ARRAY
+  
+*/
+
 numeros = [200, 300, 600]
 
 total = numeros.reduce((acumulador, atual) => acumulador + atual)
 
 console.log(total)
-
-/*
-Reduce === reduzir
-Sintaxe : 
-resultado = array.reduce((acumulador , atual) => acumulador + atual
-
-  DESAFIO : SOMAR TODOS NUMEROS DO ARRAY
-  O que espera a função Reduce : espera por 2 parametros :: 
-    (acumulador, atual) retorna acumulador <alguma Operação> atual ...guarda em uma variavel
-
-})
-
-*/
