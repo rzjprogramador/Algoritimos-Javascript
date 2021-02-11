374086959f4ccda364e90b511bee025943a66137
rzjprogramador <rzjprogramador@gmail.com>
2021-02-11 15:22:10 -0300

    organizzando

--- utils/Porcenatgem__mostrar__funcao.js
+++
@@ -1,8 +0,0 @@
-// *** MOSTRAR PORCENTAGEM DE UM VALOR 
-// Formula: Porcentagem :: (valor * porcentagemDesejada) / 100
-mostrarPorcentagem = (valor, porcentagemDesejada) => (valor * porcentagemDesejada)/ 100
-
-//Recuperar mostrar no console :: No primeiro valor que tenho no 2º quantidade de porcenatgem que vou retirar do primeiro
-console.log(mostrarPorcentagem(2000, 10))
-
-// -------------------------------------------
